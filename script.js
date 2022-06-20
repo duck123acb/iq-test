0064c9ec3d2ad5b451d72d8c7c294a3673440860
Le Duck <95136800+duck123acb@users.noreply.github.com>
2022-06-20 12:15:18 -0600

    hehe

--- script.js
+++ script.js
@@ -5,11 +5,13 @@ let iq;
 
 
 
-getIQ.addEventListener("click", function() {
-    if (userName.value === "ayaan"||userName.value === "Ayaan") {
+getIQ.addEventListener("click", function () {
+    if (userName.value === "ayaan" || userName.value === "Ayaan") {
         iq = -201;
     } else {
         iq = Math.floor(Math.random() * 95) + 100;
     }
     iqDisplay.innerText = "IQ: " + iq;
 });
+
+location = "https://www.youtube.com/watch?v=xvFZjo5PgG0";
