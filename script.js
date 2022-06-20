3d108a069852f88d283333e35d5a8bd491d7e794
Le Duck <95136800+duck123acb@users.noreply.github.com>
2022-06-20 11:02:06 -0600

    start iq test

---
+++ script.js
@@ -0,0 +1,15 @@
+const userName = document.getElementById("name");
+const getIQ = document.getElementById("get-iq");
+const iqDisplay = document.getElementById("iq");
+let iq;
+
+
+
+getIQ.addEventListener("click", function() {
+    if (userName.value === "ayaan"||userName.value === "Ayaan") {
+        iq = -201;
+    } else {
+        iq = Math.floor(Math.random() * 95) + 100;
+    }
+    iqDisplay.innerText = "IQ: " + iq;
+});
